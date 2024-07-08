725258a20db6f53f961768863de2f626e873e714
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-07-08 23:20:51 +0530

    Event Creation functionality added

--- src/app/api/createEvent/route.ts
+++ src/app/api/createEvent/route.ts
@@ -1,11 +1,13 @@
-
 import { createEvent } from "@/controllers/event.controller";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(req: NextRequest) {
+export async function POST(req: NextRequest) {
+    return createEvent(req);
+}
+
+export async function handler(req: NextRequest) {
     if (req.method === "POST") {
-        await createEvent(req);
+        return createEvent(req);
     } else {
         return NextResponse.json({ message: `Method ${req.method} Not Allowed` }, { status: 405 });
     }
