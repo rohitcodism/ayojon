eff3c4512bd0df14211cb6ebe6f1a60a8f0f9fb1
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-12 20:33:05 +0530

    feat: Add signup functionality with real-time username validation
    This commit introduces a comprehensive signup functionality to the application, incorporating several key features and improvements:
    
    1. **Real-time Username Validation**: Implemented a debounce mechanism to validate usernames in real-time as the user types. This feature checks for username uniqueness by making an asynchronous call to the `/api/uniquename` endpoint, enhancing the user experience by providing immediate feedback on username availability.
    
    2. **Form Handling with React Hook Form**: Utilized React Hook Form along with Zod for form validation, ensuring robust and scalable form management. This integration simplifies form state management and validation, making the codebase cleaner and more maintainable.
    
    3. **Dynamic Form Data Submission**: Enabled multipart/form-data submission for handling both text inputs and file uploads. This allows users to upload a profile picture during the signup process, making the application more interactive and personalized.
    
    4. **Custom Form Components**: Developed reusable form components such as `Form`, `FormField`, `FormItem`, `FormLabel`, `FormControl`, and `FormMessage` for a consistent and modular approach to building forms across the application.
    
    5. **User Feedback on Submission**: Implemented loading indicators and user feedback mechanisms during the form submission process. This includes displaying a spinner icon when checking for username availability and during the form submission, providing a clear visual cue to the user that an action is being processed.
    
    6. **Navigation Post-Signup**: Upon successful signup, users are redirected to the homepage, ensuring a seamless flow from registration to using the application.
    
    7. **Error Handling**: Added comprehensive error handling to provide feedback on any issues encountered during the username validation or signup process, improving the overall robustness of the feature.
    
    Overall, this commit significantly enhances the user registration process, making it more interactive, user-friendly, and efficient.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,274 @@
 "use client"
 
+import { signUpSchema } from "@/schemas/signup.schema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { useDebounceCallback } from "usehooks-ts"
+import axios, { AxiosError } from "axios";
+import { NextResponse } from "next/server"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Loader2 } from "lucide-react"
+import Link from "next/link";
+import { useRouter } from "next/navigation"
 
-const formSchema = z.object({
-    username: z.string().min(5).max(12),
-})
+
 
 const SignUp = () => {
 
+    const router = useRouter()
+
+    const [username, setUsername] = useState("")
+
+    const [usernameMessage, setUsernameMessage] = useState("")
+
+    const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const debounced = useDebounceCallback(setUsername, 500);
+
+    const form = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            profilePicture: undefined
+        },
+    })
+
+    useEffect(() => {
+        const checkingUsername = async () => {
+            setIsCheckingUsername(true)
+
+            setUsernameMessage("")
+
+            try {
+                const res = await axios.get(`/api/uniquename/?username=${username}`)
+
+                setUsernameMessage(res.data.message)
+            } catch (error) {
+                const axiosError = error as AxiosError<NextResponse>
+
+                console.log("Unique username error: ", axiosError);
+            }
+            finally {
+                setIsCheckingUsername(false)
+            }
+        }
+        checkingUsername()
+    }, [username])
+
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+        setIsSubmitting(true)
+
+        console.log("Function submit called!!")
+
+        try {
+
+            console.log("Form data received: ", data)
+
+            const formData = new FormData();
+
+            formData.append("username", data.username)
+            formData.append("email", data.email)
+            formData.append("password", data.password);
+
+            if(data.profilePicture != undefined){
+                formData.append("profilePicture", data.profilePicture)
+            }
+
+            console.log("formData: ",formData)
+
+
+            const res = await axios.post(`/api/signup`, formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    }
+                }
+            );
+
+            console.log("Response from Sign up API", res);
+
+            router.replace("/")
+
+        } catch (error) {
+
+            console.log("Error from signup API: ", error);
+        }
+        finally {
+            setIsSubmitting(false)
+        }
+    }
+
+    return (
+        <div
+            className="flex flex-col justify-center items-center min-h-screen bg-gray-100 gap-8 py-8"
+        >
+            <div
+                className="
+                    w-full
+                    max-w-lg
+                    p-6
+                    pb-4
+                    space-y-2
+                    bg-white
+                    rounded-lg
+                    shadow-md
+                "
+            >
+                <div
+                    className='
+                        text-center
+                    '
+                >
+                    <h1
+                        className='
+                            text-4xl
+                            font-extrabold
+                            tracking-tight
+                            lg:text-5xl
+                            mb-6
+                        '
+                    >
+                        Ayojon
+                    </h1>
+                    <p
+                        className='
+                            mb-4
+                        '
+                    >
+                        Sign up to start anonymous adventure
+                    </p>
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="what should we call you" {...field} onChange={(e) => { field.onChange(e); debounced(e.target.value) }} />
+                                    </FormControl>
+                                    {isCheckingUsername && <Loader2 className="animate-spin" />}
+                                    <p
+                                        className={`text-sm ${usernameMessage === "Username is available!!" ? "text-green-500" : "text-red-500"} font-medium`}
+                                    >
+                                        {usernameMessage}
+                                    </p>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="example@coolmail.com" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="keep it secret but mind it" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="confirmPassword"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Confirm password</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="kept it ?" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="profilePicture"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Choose a profile picture</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                        type="file" 
+                                        placeholder="give it a good look"
+                                        accept="image/*"
+                                        className="cursor-pointer"
+                                        onChange={(e) => {
+                                            const file = e.target.files ? e.target.files[0] : null
+                                            
+                                            field.onChange(file)
+                                        }} onBlur={field.onBlur} ref={field.ref} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div
+                            className="
+                            w-full
+                            flex
+                            flex-col
+                            gap-2
+                            justify-center
+                            items-center
+                            py-4
+                        "
+                        >
+                            <Button
+                                type="submit"
+                                size={"lg"}
+                                className="self-center w-full"
+                            >
+                                {
+                                    isSubmitting ? <Loader2 className="animate-spin" /> : "Sign up"
+                                }
+                            </Button>
+                            <Link
+                                href={"/signin"}
+                            >
+                                <p
+                                    className="
+                                    hover:text-black
+                                    text-gray-600
+                                    text-decoration-line: underline
+                                "
+                                >
+                                    Already a member ?
+                                </p>
+                            </Link>
+                        </div>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    );
 }
+
+export default SignUp
