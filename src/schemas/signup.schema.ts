a3a8baf67a838c5a1830be318b0e1807e9e8937c
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-12 20:34:24 +0530

    feat: Add signup schema for user registration
    This commit adds a new file, `signup.schema.ts`, which contains the schema definition for user signup. The schema includes validation rules for the username, email, password, confirmPassword, and profilePicture fields. It ensures that the email is a valid email address, the password is between 8 and 20 characters long, the confirmPassword matches the password, and the profilePicture is an optional file of type image/jpeg, image/png, or image/jpg with a size less than 10MB. Additionally, the schema checks that the password and confirmPassword fields match.
    
    This schema will be used to validate user input during the signup process, ensuring that all required fields are provided and meet the specified criteria.

---
+++ src/schemas/signup.schema.ts
@@ -0,0 +1,21 @@
+import { usernameValidation } from "./userValidation.schema";
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({ message: "Invalid email address!!" }),
+    password: z.string().min(8, "Password must be of at least 8 characters!!").max(20, "Password must be of at most 20 characters!!"),
+    confirmPassword: z.string().min(8, "Passwords does not match!!").max(20, "Passwords does not match!!"),
+    profilePicture: z.instanceof(File).optional().refine(
+        (file) => {
+            if(file){
+                return ["image/jpeg", "image/png", "image/jpg"].includes(file!.type) && file.size < 1024 * 1024 * 10
+            }
+
+            return true
+        },
+        {message: "Invalid file format or size!!"}
+    ),
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords does not match!!",
+})
