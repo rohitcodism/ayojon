cb99b0f3f8af32bd7d61d20d4c0773fc1b12c3b8
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-19 20:38:28 +0530

    feat: Update landing page HeroLight component
    This commit updates the HeroLight component on the landing page. The changes include adding a new LearnButton component, updating the background colors for light and dark themes, and modifying the text color and gradient. These updates enhance the visual appeal and user experience of the landing page.

--- src/app/components/HeroLight.tsx
+++ src/app/components/HeroLight.tsx
@@ -2,29 +2,41 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { Spotlight } from "../../components/ui/Spotlight";
 import { Button } from "@/components/ui/button";
+import { LearnButton } from "./SpecialButton";
+import Link from "next/link";
 
 export function HeroLight() {
 
     return (
-        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-white/[0.96] antialiased bg-grid-black/[0.02] relative overflow-hidden dark:bg-black/[0.96] dark:bg-grid-white/[0.02]">
             <Spotlight
                 className="-top-40 left-0 md:left-60 md:-top-20"
                 fill="white"
             />
             <div className="p-4 max-w-7xl mx-auto relative z-10 text-center">
-                <h1 className="text-5xl md:text-7xl font-extrabold text-white bg-clip-text text-transparent bg-gradient-to-b from-white to-gray-300">
+                <h1 className="text-5xl md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-b dark:from-white dark:to-gray-300 from-black to-gray-600 py-2">
                     Welcome to Ayojon
                 </h1>
-                <p className="mt-4 font-medium text-lg md:text-2xl text-gray-200 max-w-2xl mx-auto">
+
+                <p className="mt-4 font-medium text-lg md:text-2xl dark:text-gray-200 max-w-2xl mx-auto text-gray-900">
                     Your Ultimate Event Management Solution
                 </p>
-                <div className="mt-8 flex justify-center gap-4">
-                    <Button className="bg-white text-black py-3 px-6 rounded-lg text-lg font-semibold shadow-md">
-                        Get Started
-                    </Button>
-                    <Button className="bg-transparent border border-white text-white py-3 px-6 rounded-lg text-lg font-semibold shadow-md">
-                        Learn More
-                    </Button>
+                <div className="mt-8 flex justify-center gap-6">
+                    <Link
+                        href={"/signup"}
+                    >
+                        <Button className="dark:bg-white dark:text-black py-3 px-6 rounded-lg text-lg font-semibold shadow-md bg-black text-white">
+                            Get Started
+                        </Button>
+                    </Link>
+                    <Link href={"/about"}>
+                        <Button
+                            variant={"outline"}
+                            className="bg-transparent rounded-full dark:text-white py-4 px-6 text-lg font-semibold shadow-md text-black border-2 border-black hover:bg-gray-200"
+                        >
+                            Create event
+                        </Button>
+                    </Link>
                 </div>
             </div>
         </div>
