478ac907dc8632eee5d1bced1c8e794a5a4fda7c
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-19 20:38:15 +0530

    refactor: Remove unused ThemeCTXProvider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/ui/navbar";
 import { HeroLight } from "./components/HeroLight";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ThemeCTXProvider } from "@/context/ThemeContext";
 
 
 
@@ -9,10 +8,8 @@ export default function Home() {
   return (
     <div className="flex flex-col dark:bg-black min-h-screen">
       <ThemeProvider>
-        <ThemeCTXProvider>
           <Navbar />
           <HeroLight />
-        </ThemeCTXProvider>
       </ThemeProvider>
     </div>
   );
