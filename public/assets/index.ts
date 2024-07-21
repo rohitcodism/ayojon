86975cc8493bedc91b388f17d2f27d78916d08b4
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-21 09:00:26 +0530

    feat: Add SVG icons and index file for assets
    This commit adds SVG icons and an index file for assets. The SVG icons include the Google icon, which can be used for various purposes in the application. The index file exports the SVG icons and other image assets, such as music, tech, stand-up, and yoga images. These additions enhance the application by providing a centralized location for accessing and utilizing assets.

---
+++ public/assets/index.ts
@@ -0,0 +1,13 @@
+import GoogleIcon from "./icons8-google.svg"
+import musicEventImage from "./music.jpg"
+import techEventImage from "./tech.jpg"
+import standUpImage from "./stand-up.jpg"
+import yogaImage from "./yoga.jpg"
+
+export {
+    GoogleIcon,
+    musicEventImage,
+    techEventImage,
+    standUpImage,
+    yogaImage,
+}
