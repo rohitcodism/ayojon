95a0892d648052c888f943bd3a9fdcb4fa9faa91
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-21 09:13:06 +0530

    feat: Add react-intersection-observer package for lazy loading images
    This commit adds the react-intersection-observer package to enable lazy loading of images in the application. Lazy loading improves page load performance by only loading images when they are in the viewport, reducing initial load time. This enhancement enhances the user experience by optimizing image loading and improving overall performance.

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -1,9 +1,40 @@
+"use client"
+import { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { eventTypes } from "../../../constants";
 import { EventCard } from "./EventCard";
 
-
-
 const Events = () => {
+    const controls = useAnimation();
+    const { ref, inView } = useInView({
+        threshold: 0.1
+    });
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        } else {
+            controls.start("hidden");
+        }
+    }, [controls, inView]);
+
+    const containerVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                staggerChildren: 0.2
+            }
+        }
+    };
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: { opacity: 1, y: 0 }
+    };
+
     return (
         <div
             className="
@@ -14,45 +45,27 @@ const Events = () => {
                 py-4
             "
         >
-            <div
-                className="flex flex-col gap-2 pb-2"
-            >
-                <h1
-                    className="text-4xl font-bold"
-                >
-                    Discover the best events
-                </h1>
-                <p
-                    className="text-md font-semibold"
-                >
-                    We offer a wide range of events, from music festivals and tech conferences and stand up comedy shows
+            <div className="flex flex-col gap-2 pb-2">
+                <h1 className="text-4xl font-bold">Discover the best events</h1>
+                <p className="text-md font-semibold">
+                    We offer a wide range of events, from music festivals and tech conferences to stand-up comedy shows
                 </p>
             </div>
-            <div
-                className="
-                    flex
-                    items-center
-                    gap-16
-                    pt-8
-                "
+            <motion.div
+                className="flex items-center gap-16 pt-8"
+                ref={ref}
+                variants={containerVariants}
+                initial="hidden"
+                animate={controls}
             >
-                {
-                    eventTypes.map((eventType) => {
-                        return(
-                            <EventCard title={eventType.title} image={eventType.image} />
-                        );
-                    })
-                }
-            </div>
-            {
-                /*
-                    <div>
-                        Fetch featured events from DB
-                    </div>
-                 */
-            }
+                {eventTypes.map((eventType) => (
+                    <motion.div key={eventType.title} variants={itemVariants} whileHover={{ scale: 1.05 }}>
+                        <EventCard title={eventType.title} image={eventType.image} />
+                    </motion.div>
+                ))}
+            </motion.div>
         </div>
     );
-}
+};
 
 export default Events;
