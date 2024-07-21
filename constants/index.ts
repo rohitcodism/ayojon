c6b03cabedd746a2edabda62ea9c2f42726ec313
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-21 09:00:13 +0530

    feat: Add event types to constants
    This commit adds the event types to the constants file. The event types include music & festivals, tech & business, comedy & theaters, and health & wellness. This addition enhances the application by providing predefined event types for better organization and filtering of events.

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+import { musicEventImage, standUpImage, techEventImage, yogaImage } from "../public/assets";
+
+
+const eventTypes = [
+    {
+        id: 1,
+        title: "Music & Festivals",
+        image: musicEventImage
+    },
+    {
+        id: 2,
+        title: "Tech & Business",
+        image: techEventImage
+    },
+    {
+        id: 3,
+        title: "Comedy & Theaters",
+        image: standUpImage
+    },
+    {
+        id: 4,
+        title: "Health & Wellness",
+        image: yogaImage
+    }
+]
+
+export {
+    eventTypes
+}
