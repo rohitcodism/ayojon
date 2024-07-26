db0b93891c757137529f21663154cca9fba5f637
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-26 08:49:03 +0530

    feat: Add SearchBox component to home page
    This commit adds the SearchBox component to the home page, allowing users to search for specific content within the application. The SearchBox component enhances the user experience by providing a convenient way to search for information and improves the overall functionality of the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Events from "./components/Events";
 import { Community } from "./components/Community";
 import { Featured } from "./components/Featured";
+import { SearchBox } from "./components/SearchBox";
 
 
 
@@ -12,6 +13,7 @@ export default function Home() {
     <div className="flex flex-col dark:bg-black min-h-screen gap-12 pb-8">
       <ThemeProvider>
           <HeroLight />
+          <SearchBox />
           <Featured />
           <Events />
           <Community />
