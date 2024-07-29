4faeb0445cff4d2733a208c210b82bd8f3bf0597
rohitcodism <rohitpaulhhs04@gmail.com>
2024-07-29 16:33:40 +0530

    refactor: Move Navbar component outside of EventLayout

--- src/app/(app)/events/components/Navbar.tsx
+++ src/app/(app)/events/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link"
+import { SearchBox } from "./EventSearch";
+import { Button } from "@/components/ui/button";
 
 
 
 export const Navbar = () => {
     return (
-        <div
-            className="container flex items-center gap-8 py-6"
-        >
-            <div className=" container py-3 flex justify-between items-center">
-                <div className="text-xl font-bold text-black dark:text-white">
+        <nav className="py-6 bg-white shadow-md dark:bg-black">
+            <div className="flex container gap-8 items-center w-full dark:bg-black">
+                <div className="text-2xl font-bold text-black dark:text-white">
                     <Link href="/">Ayojon</Link>
                 </div>
+                <SearchBox />
+                <Button
+                    variant="outline"
+                    className="dark:bg-white bg-black rounded-full dark:text-black py-4 px-4 text-lg font-semibold shadow-md text-white border-2 border-black dark:border-white hover:bg-gray-200 dark:hover:bg-transparent dark:hover:text-white hover:text-black"
+                >
+                    Create Event
+                </Button>
             </div>
-        </div>
+        </nav>
     );
 }
