3c1b04938428a28b00a4c0ab3729efe5a68dc9c6
rohitcodism <rohitpaulhhs04@gmail.com>
2024-08-11 11:02:14 +0530

    feat: Implement event search functionality

--- src/app/(app)/events/components/EventSearch.tsx
+++ src/app/(app)/events/components/EventSearch.tsx
@@ -1,16 +1,37 @@
 "use client"
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { Event } from "@/models/event.model";
+import axios from "axios";
 import { Search } from "lucide-react";
-
-/*
-    TODO: Yet to Implement search API
-*/
+import { useState } from "react";
 
 
 
 export const SearchBox = () => {
+
+    const[searchKeyword, setSearchKeyWord] = useState<string | null>(null)
+
+    const[searchResult, setSearchResult] = useState<Event[] | null>();
+
+    const {toast} = useToast();
+
+    const getSearchResult = async(searchKeyword: string) => {
+        const res = await axios.get("");
+
+        if(res.status === 200){
+            setSearchResult(res.data);
+        }else{
+            toast({
+                title: "Oops!!",
+                description: "Something went wrong.",
+                variant: "destructive"
+            })
+        }
+    }
+
     return (
-        <div className="container flex justify-center items-center" >
+        <div className="container flex flex-col justify-center items-center" >
             <div
                 className="relative max-w-[500px] w-full"
             >
@@ -30,9 +51,19 @@ export const SearchBox = () => {
                     text-sm
                 "
                     style={{ outline: 'none', boxShadow: 'none' }}
+                    onChange={(e) => setSearchKeyWord(e.target.value)}
                 />
                 <Search size={20} className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white" />
             </div>
+            {
+                    searchKeyword && (
+                        <div
+                            className="bg-gray-900 rounded-lg py-4 px-4 max-w-[450px] h-auto w-full text-center"
+                        >
+                            Nothing to show up
+                        </div>
+                    )
+                }
         </div>
     );
 }
