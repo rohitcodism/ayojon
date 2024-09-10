57628813d8af9c55498b38f453f60f84f1b46765
rohitcodism <rohitpaulhhs04@gmail.com>
2024-09-10 15:58:42 +0530

    refactor: Adjust caret-blink animation in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,10 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
         spotlight: {
           "0%": {
             opacity: "0",
@@ -82,6 +86,7 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
       },
     },
   },
