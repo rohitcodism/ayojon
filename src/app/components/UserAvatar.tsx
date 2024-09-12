1c315b82bdb5ebde6d4bdeb5ae80f20724dc575d
rohitcodism <rohitpaulhhs04@gmail.com>
2024-09-12 08:49:15 +0530

    feat: Add UserAvatar component to Navbar

---
+++ src/app/components/UserAvatar.tsx
@@ -0,0 +1,35 @@
+"use client"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { signOut, useSession } from "next-auth/react"
+import { LogOut } from "lucide-react"
+
+
+export const UserAvatar = () => {
+
+    const { data: session,status } = useSession();
+
+    return (
+        <div className="flex justify-center items-center">
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar className="w-10 h-10">
+                        <AvatarImage src={session?.user?.image || "https://github.com/shadcn.png"} />
+                        <AvatarFallback>{session?.user.name}</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-black">
+                    <DropdownMenuItem className="cursor-pointer focus:bg-white/20">Profile</DropdownMenuItem>
+                    <DropdownMenuItem className="flex items-center gap-4 cursor-pointer focus:bg-white/20 focus:text-red-700" onClick={() => signOut()}>Log out <LogOut size={15} /></DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
+}
