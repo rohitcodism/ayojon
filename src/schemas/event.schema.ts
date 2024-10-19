1f5fa0a6295cb3748949ba5560d16db3a0880a8a
rohitcodism <rohitpaulhhs04@gmail.com>
2024-10-20 00:15:11 +0530

    refactor: Update event schema for event creation
    - Update the maximum length of the event name to 50 characters
    - Adjust the maximum length of the event description to 300 characters
    - Modify the category validation to allow all categories from the constant
    - Add capacity and price fields to the event schema
    - Add optional organizers and speakers fields to the event schema
    - Add banner field to the event schema with file type and size validation

--- src/schemas/event.schema.ts
+++ src/schemas/event.schema.ts
@@ -2,6 +2,7 @@ import { DefaultSession } from 'next-auth';
 import { z } from 'zod'
 import { eventCategories } from '../../constants'
 import { userSchema } from './user.schema'
+import { User } from '@/models/user.model';
 
 const allowedCategories = [    
     "Music",
@@ -31,14 +32,26 @@ const allowedCategories = [
 const defaultEventDate: Date = new Date(Date.now() + 24*3600000)
 
 const eventSchema = z.object({
-    eventName: z.string().min(5, "Event name must contain 5 letters.").max(20, "Event name can contain at max 20 letters."),
+    eventName: z.string().min(5, "Event name must contain 5 letters.").max(50, "Event name can contain at max 20 letters."),
     description: z.string().min(10, "Event description must contain at least 10 words.").max(300, "Event description can at most contain 300 letters."), //TODO: Adjust it according to its need
-    category: z.enum(allowedCategories),
+    category: z.enum([...allowedCategories]),
     date: z.date().default(defaultEventDate),
     location: z.string(),
-    capacity: z.number(),
-    organizers: z.string(),
-    speakers: z.string()
+    capacity: z.string(),
+    price: z.string(),
+    //TODO: Work left on this
+    // organizers: z.array(userSchema).optional(),
+    // speakers: z.array(userSchema).optional(),
+    banner: z.instanceof(File).optional().refine(
+        (file) => {
+            if(file){
+                return ["image/jpeg", "image/png", "image/jpg"].includes(file!.type) && file.size < 1024 * 1024 * 10
+            }
+
+            return true
+        },
+        {message: "Invalid file format or size!!"}
+    ),
 })
 
 export {
